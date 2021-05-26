c3589d0dd0c2131114689cb24558c00e52dbaece
tmd22121999 <37409790+tmd22121999@users.noreply.github.com>
2021-05-26 16:38:40 +0700

    Add files via upload

---
+++ tmd221299/btlweb2/api/login.php
@@ -0,0 +1,31 @@
+<?php
+
+header("Access-Control-Allow-Methods:POST");
+include_once($_SERVER['DOCUMENT_ROOT']."/btlweb2/model/userModel.php");
+$errorMess=["result"=>0,"mess"=>"Đăng nhập thất bại"];
+    $input = json_decode(file_get_contents('php://input'),true);
+    if(isset($input["Phone"]) || isset($input["Password"])){
+
+        $where = " WHERE ";
+        $where=$where."Phone = '".$input["Phone"]."'";
+        $where=$where." AND Password = '".$input["Password"]."'";
+        
+        $db=new DB();
+        $db->connect();
+        $result = $db->SeclectData("`user`","id",$where);
+        
+        if(!empty($result)){
+            $Authorization= base64_encode($input["Phone"].":".$input["Password"]);
+            $mess=["result"=> 1 ,"messenger"=>"đăng nhập thành công"];
+            $mess["Authorization"]=$Authorization;
+            echo json_encode($mess,JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE );
+        }else{
+            echo json_encode($errorMess,JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE );
+        }
+        
+    } else{
+        echo json_encode($errorMess,JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE );
+    }
+
+
+?>
